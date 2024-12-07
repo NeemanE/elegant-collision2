762d57ce7b6cd3455c1e19710f66f200081e69ae
Neeman E <110888161+NeemanE@users.noreply.github.com>
2024-12-06 19:49:08 -0600

    fixed mobile

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ const Hero = () => (
     <>
       {/* Hero Image Section */}
       <div
-        className="hero h-[50vh] bg-cover bg-center"
+        className="hero h-[15vh] sm:h-[40vh] md:h-[50vh] bg-cover bg-center"
         style={{ backgroundImage: "url('/images/logo.png')" }}
       >
         <div className="hero-overlay bg-opacity-0"></div>
@@ -11,7 +11,7 @@ const Hero = () => (
       {/* Text and Button Section */}
       <div className="text-center py-10 text-white">
         <div className="max-w-md mx-auto">
-          <p className="mb-5 text-lg">
+          <p className="mb-5 text-xl">
             Exceptional Customer Service and Outstanding Auto Body Repairs.
           </p>
           <button className="btn btn-primary">Contact Us</button>
